16d6fcb4f9ec56fc04a4e123bb2bb11d64cec218
Anhongbum <anbum8463@gmail.com>
2020-01-12 15:25:14 +0900

    Updates

---
+++ precache-manifest.8a882227973ef6ebeed9d8eb652273ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8b2ed84c390b4361b736b377280a55",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "baaa9aab4eb8e5e07b10",
+    "url": "/movie_app_2019/static/css/main.21bc3d3e.chunk.css"
+  },
+  {
+    "revision": "a877f66de75b7ddb4110",
+    "url": "/movie_app_2019/static/js/2.3f7774be.chunk.js"
+  },
+  {
+    "revision": "baaa9aab4eb8e5e07b10",
+    "url": "/movie_app_2019/static/js/main.30b61226.chunk.js"
+  },
+  {
+    "revision": "26c5953caa52b1651c1d",
+    "url": "/movie_app_2019/static/js/runtime-main.60e209e0.js"
+  }
+]);
